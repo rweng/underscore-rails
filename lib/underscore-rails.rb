a7c19cff596c06702d115f1f58f569718b7be74d
Costis Panagiotopoulos <costisp@gmail.com>
2013-01-17 12:19:36 +0000

    Fix formatting - convert tabs to spaces.

--- lib/underscore-rails.rb
+++ lib/underscore-rails.rb
@@ -3,9 +3,9 @@ require "underscore-rails/version"
 module Underscore
   module Rails
     if defined?(::Rails) and ::Rails.version >= "3.1"
-			class Rails::Engine < ::Rails::Engine
-				# this class enables the asset pipeline
-			end
-		end
+      class Rails::Engine < ::Rails::Engine
+        # this class enables the asset pipeline
+      end
+    end
   end
 end
