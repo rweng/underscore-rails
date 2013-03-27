0a1ddfdc21d7b85d8993bcddef70f4ed65cbdf60
Robin Wenglewski <robin@wenglewski.de>
2013-03-27 17:31:43 +0100

    use Gem::Requirement#satisfied_by?

--- lib/underscore-rails.rb
+++ lib/underscore-rails.rb
@@ -2,7 +2,7 @@ require "underscore-rails/version"
 
 module Underscore
   module Rails
-    if defined?(::Rails) and Gem::Version.new(::Rails.version) >= Gem::Version.new("3.1")
+    if defined?(::Rails) and Gem::Requirement.new('>= 3.1').satisfied_by?(Gem::Version.new Rails.version)
       class Rails::Engine < ::Rails::Engine
         # this class enables the asset pipeline
       end
